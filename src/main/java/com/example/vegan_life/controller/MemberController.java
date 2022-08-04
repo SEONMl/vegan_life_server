602a52805aaba13806752e6d8fbd7e28a40bcfb1
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-08-04 23:41:39 +0900

    feat: #7

--- src/main/java/com/example/vegan_life/controller/MemberController.java
+++ src/main/java/com/example/vegan_life/controller/MemberController.java
@@ -1,8 +1,29 @@
 package com.example.vegan_life.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.vegan_life.dto.MemberDto;
+import com.example.vegan_life.entity.MemberEntity;
+import com.example.vegan_life.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class MemberController {
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping("/member")
+    public ResponseEntity getMemberInfo(@RequestBody MemberDto dto){
+        MemberEntity result = memberService.getMemberInfo(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PatchMapping("/member")
+    public ResponseEntity modifyMemberInfo(@RequestBody MemberDto dto){
+        MemberEntity result = memberService.modifyMemberInfo(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 
 }
--- src/main/java/com/example/vegan_life/service/MemberService.java
+++ src/main/java/com/example/vegan_life/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.example.vegan_life.service;
 
+import com.example.vegan_life.dto.MemberDto;
+import com.example.vegan_life.entity.MemberEntity;
 import com.example.vegan_life.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,18 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     @Autowired
     MemberRepository memberRepository;
+
+    public MemberEntity getMemberInfo(MemberDto dto) {
+        MemberEntity target = memberRepository.findById(dto.getMember_id()).orElse(null);
+        return target;
+    }
+
+    public MemberEntity modifyMemberInfo(MemberDto dto) {
+        MemberEntity target =memberRepository.findById(dto.getMember_id()).orElse(null);
+        if (target != null){
+            //target.nickname = dto.getNickname();
+
+        }
+        return target;
+    }
 }
