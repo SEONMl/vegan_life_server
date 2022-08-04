2a3693957b161fe5b56db9cae0b656cd5f0eda7a
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-08-04 23:39:15 +0900

    feat: add configurations

---
+++ src/main/java/com/example/vegan_life/security/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.example.vegan_life.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        //@PropertySource("configs/application-dev.properties"),
+})
+public class PropertyConfig {
+}
---
+++ src/main/java/com/example/vegan_life/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.vegan_life.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .headers().disable()
+                .cors().disable()
+                .csrf().disable()
+                .exceptionHandling();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
