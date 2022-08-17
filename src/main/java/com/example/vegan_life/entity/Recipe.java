fe214fbcc55f96ee95a21b257527f9ce2fd6d29e
SEONMl <sm000117@naver.com>
2022-08-17 20:54:36 +0900

    feat: #5 Recipe Entity 수정

--- src/main/java/com/example/vegan_life/entity/Recipe.java
+++ src/main/java/com/example/vegan_life/entity/Recipe.java
@@ -1,7 +1,10 @@
 package com.example.vegan_life.entity;
 
 import com.sun.istack.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
