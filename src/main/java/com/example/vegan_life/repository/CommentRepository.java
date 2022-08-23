54b1a79509ec380c9c5a26a2991871c0f54890eb
SEONMl <sm000117@naver.com>
2022-08-23 22:10:54 +0900

    fix: 오타 수정

--- src/main/java/com/example/vegan_life/repository/CommentRepository.java
+++ src/main/java/com/example/vegan_life/repository/CommentRepository.java
@@ -1,10 +1,7 @@
 package com.example.vegan_life.repository;
 
-import com.example.vegan_life.entity.CommentLike;
 import com.example.vegan_life.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface CommentRepository extends JpaRepository<Comments,Long> {
 
-    public Optional<List<Comments>> findAllByArticleId(@Param("article_id")Long article_id);
-
+    public Optional<List<Comments>> findAllByArticleId(Long article_id);
 }
